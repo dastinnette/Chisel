fd40567b89b0adf303efe547e5cbddd6c836f394
David Stinnette <dastinnette@gmail.com>
2015-06-09 19:14:27 -0600

    Converts markdown to html

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -4,7 +4,9 @@ require 'minitest/pride'
 require_relative '../lib/chisel'
 
 class ChiselTest < Minitest::Test
-  def test_read_a_file
-    skip
+  def test_chisel_converts_markdown_to_html
+    markdown = "#i am a header\n\ni am a paragraph"
+    html     = "<h1>i am a header</h1>\n\n<p>i am a paragraph</p>"
+    assert_equal html, Chisel.new(markdown).to_html
   end
 end
